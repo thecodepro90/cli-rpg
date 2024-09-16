ffeee9e649a0a2b4dc08f1173dec8f95d1a5c319
thecodepro90 <97404537+thecodepro90@users.noreply.github.com>
2024-09-16 17:31:46 +0000

    Add level map, player map, MapController class, fix movement

--- main.py
+++ main.py
@@ -4,9 +4,23 @@ class Sprite:
         self.y = y
         self.name = name
         
-player1 = Sprite(0, 0, "Jane Doe")        
+player1 = Sprite(2, 3, "Jane Doe")    
 
-map = [
+
+class MapController:
+    def __init__(self, map):
+        self.map = map
+
+    def get_map(self):
+        return self.map
+    
+    def get_value(self, x, y):
+        return self.map[y][x]
+    
+    def set_value(self, x, y, value):
+        self.map[y][x] = value
+
+level_map = [
     ["0","0","0","0","0","0"],
     ["0","0","0","0","0","0"],
     ["0","0","0","0","0","0"],
@@ -15,27 +29,50 @@ map = [
     ["0","0","0","0","0","0"],
 ]
 
-map[player1.y][player1.x] = "1"
+player_map = [
+    ["0","0","0","0","0","0"],
+    ["0","0","0","0","0","0"],
+    ["0","0","0","0","0","0"],
+    ["0","0","0","0","0","0"],
+    ["0","0","0","0","0","0"],
+    ["0","0","0","0","0","0"],
+]
+
+player_map_controller = MapController(player_map)
+level_map_controller = MapController(level_map)
 
 while True:
     print("\n\n\n\n\n\n")
     print(player1.x, player1.y)
-    map[abs(player1.y)][abs(player1.x)] = "1"
-    p_map = ""
-    for i in map:
+    print(level_map)
+    print(player_map)
+    print()
+    player_map_controller.set_value(player1.x, player1.y, "1")
+
+    for i in player_map_controller.get_map():
         for j in i:
-            p_map += j+"  "
-        p_map += "\n"
-    print(p_map)
+            print(j+" ", end="")
+        print()
+
+    print(level_map)
+    print(player_map)
+
+
+
     move = input(": ")
         
     if move == "w":
-        player1.y += 1
+        player_map_controller.set_value(player1.x, player1.y, level_map_controller.get_value(player1.x, player1.y))
+        player1.y -= 1
+        
     elif move == "a":
+        player_map_controller.set_value(player1.x, player1.y, level_map_controller.get_value(player1.x, player1.y))
         player1.x -= 1
     elif move == "s":
-        player1.y -= 1
+        player_map_controller.set_value(player1.x, player1.y, level_map_controller.get_value(player1.x, player1.y))
+        player1.y += 1
     elif move == "d":
+        player_map_controller.set_value(player1.x, player1.y, level_map_controller.get_value(player1.x, player1.y))
         player1.x += 1
         
     
