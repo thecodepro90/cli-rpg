bd48b54c0cebbd99087aab042a8990358daf98d0
thecodepro90 <97404537+thecodepro90@users.noreply.github.com>
2024-09-24 17:24:29 +0000

    Added logger

---
+++ logger.py
@@ -0,0 +1,84 @@
+"""
+logger
+
+Description:
+"""
+import time
+import sys
+
+"""Logger init function.
+
+Keyword arguments:
+Return: prints initialization information and writes to log
+"""
+def logger_init():
+    # All time data
+    named_tuple = time.localtime()
+    
+    # Full time string: year-month-day hour:minute:second
+    time_string = time.strftime("%Y-%m-%d %H:%M:%S", named_tuple)
+    # Current function ran in
+    function_name = sys._getframe(1).f_code.co_name
+    # Line number where function was called
+    line_number = sys._getframe(1).f_lineno
+    # Current file name
+    filename = sys._getframe(1).f_code.co_filename
+    
+    # Full string: year-month-day hour:minute:second | INIT | filename:function_name:line_number - LOGGER INITIALIZED
+    full_string = time_string + " "+"| INIT |"+" "+filename+":"+function_name+":"+str(line_number)+" - "+"LOGGER INITIALIZED \n"
+    
+    # Writes full string to logs
+    with open("logs.txt", "a") as f:
+        f.write(full_string)
+        
+    # Prints full string in console
+    print(full_string)
+        
+    
+
+"""Log function
+
+Keyword arguments:
+message -- log message: string
+log_file -- where to write log: string (file, console, both)
+Return: prints the debug message in console or in log based on log_file variable
+"""
+def log(message, log_file):
+    # All time data
+    named_tuple = time.localtime()
+    
+    # Full time string: year-month-day hour:minute:second
+    time_string = time.strftime("%Y-%m-%d %H:%M:%S", named_tuple)
+    
+    # Current function ran in
+    function_name = sys._getframe(1).f_code.co_name
+    
+    # Line number where function was called
+    line_number = sys._getframe(1).f_lineno
+    
+    # Current file name
+    filename = sys._getframe(1).f_code.co_filename
+    
+    # Full string: year-month-day hour:minute:second | DEBUG | filename:function_name:line_number - LOGGER INITIALIZED
+    full_string = time_string + " "+"| DEBUG |"+" "+filename+":"+function_name+":"+str(line_number)+" - "+message
+    
+    # If log_file equals 'file'
+    if log_file == "file":
+        # Write full string to file
+        with open("logs.txt", "a") as f:
+            f.write(full_string+"\n")
+            
+    # If log_file equals console print full string
+    elif log_file == "console":
+        print(full_string)
+        
+    # If log_file equals both then print and write to file
+    elif log_file == "both":
+        with open("logs.txt", "a") as f:
+            f.write(full_string+"\n")
+            
+        print(full_string)
+    
+    
+
+    
